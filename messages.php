c76c8db94f686868386fe38ae0734c7bc93d6689
Luis Munoz <lmunoz0806@gmail.com>
2022-02-13 19:09:57 -0500

    cleaning up repo

--- messages.php
+++ messages.php
@@ -29,12 +29,7 @@ $port = 5432;
 
 // Create connection
 $conn = pg_connect("host=$host port=$port dbname=$db user=$db_user password=$db_pass");
-// Check connecion 
-if ($conn) {
-    // echo "Connection attempt succeeded. \n";
-    } else {
-    // echo "Connection attempt failed. \n"; 
-    }
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 $token = null;
@@ -62,20 +57,20 @@ if(isset($headers['Authorization'])){
 
 function token_to_id($conn,$token)
 {
-         $query = "SELECT u_id,phone_number FROM \"public\".\"Login\" WHERE token = '".$token."' ;";
-                 $result = pg_query($conn, $query);
-                 if ($row = pg_fetch_assoc($result)) {
-                    $u_id = $row['u_id'];
-                    $phone_number = $row['phone_number'];
-                    // $query2 = "SELECT * FROM \"public\".\"Messages\" WHERE s_id = '".$u_id."' ;";
-                    // $response = pg_query($conn, $query2);
-                    // if($row =  pg_fetch_assoc($response)){
-                    //       $message = $row['body_text'];
-                             # need to look  throuhg read messages
-                    //}
-                 }
-                 return($u_id);
-                 // print_r($row);
+   $query = "SELECT u_id,phone_number FROM \"public\".\"Login\" WHERE token = '".$token."' ;";
+      $result = pg_query($conn, $query);
+      if ($row = pg_fetch_assoc($result)) {
+         $u_id = $row['u_id'];
+         $phone_number = $row['phone_number'];
+         // $query2 = "SELECT * FROM \"public\".\"Messages\" WHERE s_id = '".$u_id."' ;";
+         // $response = pg_query($conn, $query2);
+         // if($row =  pg_fetch_assoc($response)){
+         //       $message = $row['body_text'];
+                  # need to look  throuhg read messages
+         //}
+      }
+      return($u_id);
+      // print_r($row);
 }
 
 ?>
