3924ffc8fbc6f9356977e9f8a763cf011d4f483a
LukasAVB <lukasavb@gmail.com>
2022-02-19 16:28:27 -0500

    chore: CORS permissions

--- auth.php
+++ auth.php
@@ -1,6 +1,9 @@
 <?php
 require('./config.php');
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
 if($method === "POST"){
 	$output = array();
 	$purpose = $_GET['purpose'];
