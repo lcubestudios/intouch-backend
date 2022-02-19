f8e36a4d480e2726ba569837b04574f340b39d2f
Luis Munoz <lmunoz0806@gmail.com>
2022-02-19 17:25:51 -0500

    sending token via postman

--- contact.php
+++ contact.php
@@ -7,20 +7,22 @@ if($method === "GET"){
     $contacts_table = 'public.contacts'
 
     // Load Token
-    $token = 1234567890;
+    $raw=file_get_contents('php://input');
+    $data=json_decode($raw,true);
+    $token = $data['token'];
     $query = "SELECT u_id FROM " . $users_table. " WHERE token = '". $token. "'";
     $result = pg_query($conn, $query);
-
+    
     // Load User ID
     if($row = pg_fetch_assoc($result)){
         $u_id = $row['u_id'];
     
     // Load User Contacts 
-    $query2 = "SELECT c_uid FROM " . $contacts_table. " WHERE u_id = '". $u_id. "'";
-    $data = pg_query($conn, $query2);
-    while ($row2 = pg_fetch_row($data)){
-        var_dump($row2);
-    }
+    // $query2 = "SELECT c_uid FROM " . $contacts_table. " WHERE u_id = '". $u_id. "'";
+    // $data = pg_query($conn, $query2);
+    // while ($row2 = pg_fetch_row($data)){
+    //     var_dump($row2);
+    // }
         $output = array(
             'status_code' => 200,
             'id' => $u_id
