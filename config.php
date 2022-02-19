a3f1786066dedfeb42f2f624f3f76ba64259b5e5
LukasAVB <lukasavb@gmail.com>
2022-02-19 12:58:01 -0500

    test: auth post

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// Report all errors
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+##COMPOSER NEEDED!##
+require_once realpath(__DIR__ . '/vendor/autoload.php');
+
+// Looing for .env at the root directory
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Retrive env variable
+$host = $_ENV['HOST'];
+$db_user = $_ENV['DB_USER'];
+$db_pass = $_ENV['DB_PASSWORD'];
+$db = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
+
+// Create connection
+$conn = pg_connect("host=$host port=5432 dbname=$db user=$db_user password=$db_pass");
+// Check connecion 
+if ($conn) {
+    echo "Connection attempt succeeded. \n";
+    } else {
+    echo "Connection attempt failed. \n"; 
+}
+
+$method = $_SERVER['REQUEST_METHOD'];
+php?>
