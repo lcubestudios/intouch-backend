db0b98173c8d9f896c0af389320c37573d4d2fa3
Luis Munoz <lmunoz0806@gmail.com>
2022-02-19 17:06:44 -0500

    testing list of contacts

--- contact.php
+++ contact.php
@@ -3,17 +3,27 @@ require('./config.php');
 
 if($method === "GET"){
     $output =  array();
-    $table = 'public.users';
+    $users_table = 'public.users';
+    $contacts_table = 'public.contacts'
 
     // Load Token
     $token = 1234567890;
-    $query = "SELECT u_id FROM " . $table. " WHERE token = '". $token. "'";
+    $query = "SELECT u_id FROM " . $users_table. " WHERE token = '". $token. "'";
     $result = pg_query($conn, $query);
 
+    // Load User ID
     if($row = pg_fetch_assoc($result)){
+        $u_id = $row['u_id']
+    
+    // Load User Contacts 
+    $query2 = "SELECT c_uid FROM " . $contacts_table. " WHERE u_id = '". $u_id. "'";
+    $data = pg_query($conn, $query2);
+    while ($row2 = pg_fetch_row($data)){
+        var_dump($row2);
+    }
         $output = array(
             'status_code' => 200,
-            'id' => $row['u_id']
+            'id' => $u_id
         );
     }
     
