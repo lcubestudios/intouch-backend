1f58d9b807ab1b804b1af61e5f761e80c3d95a89
dennys9415 <dennys9415@gmail.com>
2022-02-19 18:52:48 -0500

    delete api updated

--- delete.php
+++ delete.php
@@ -5,8 +5,9 @@ require('./config.php');
 
 switch ($method):
     case 'GET':
-        echo("hello get");
+        echo "";
         Getting();
+        echo"";
         break;    
     case 'DELETE':
         echo("Deleting the user guy \n");
@@ -16,16 +17,45 @@ endswitch;
 
 function Getting(){
     require('./config.php');
+    $table = 'public.users';
+    $table3 = 'public.messages';
 
     $raw=file_get_contents('php://input');
     $data=json_decode($raw,true);
-    $phone_number = $data['phone_number'];
+    $token = $data['token'];
 
-    $query = "SELECT * FROM users WHERE phone_number = '".$phone_number."';";
+    $query = "SELECT u_id FROM " . $table ." WHERE token = '".$token."';";
     $result = pg_query($conn, $query);
-    while ($row = pg_fetch_row($result)) {
-    //    echo "$row[0] $row[1] $row[2] $row[3] $row[4] $row[5]\n";
-        echo "$row[0]\n";
+    while ($rowing = pg_fetch_row($result)) {
+        $u_id = "$rowing[0]";
+    }
+
+    $query2 = "SELECT body_text,date,r_read,r_id FROM " . $table3 ." WHERE s_id = '".$u_id."';";
+    $result2 = pg_query($conn, $query2);
+    while ($row = pg_fetch_row($result2)) {
+        echo "\n the u_id is: ";
+        echo $u_id;
+        $messages = "$row[0]";
+        echo "\nthe message is: ";
+        echo $messages;
+        $date_message = "$row[1]";
+        echo "\nthe date is: ";
+        echo $date_message;
+        $r_read = "$row[2]";
+        echo "\nthe Message was read: ";
+        echo $r_read;
+        $r_id = "$row[3]";
+        echo "\nthe receiber id is: ";
+        echo $r_id;
+
+        $query3 = "SELECT phone_number FROM " . $table ." WHERE u_id = '".$r_id."';";
+        $result3 = pg_query($conn, $query3);
+        while ($rowe = pg_fetch_row($result3)) {
+            $phone_number = "$rowe[0]";
+            echo "\nthe phone number is: ";
+            echo $phone_number;
+            echo "\n";
+        }
     }
 
 }
