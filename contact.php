7e2ee4659661c18950ae10043de3ea39d762dbc2
Luis Munoz <lmunoz0806@gmail.com>
2022-02-19 18:11:45 -0500

    updating r

--- contact.php
+++ contact.php
@@ -20,7 +20,7 @@ if($method === "GET"){
     $query2 = "SELECT c_uid FROM " . $contacts_table. " WHERE u_id = '". $u_id. "'";
     $result2 = pg_query($conn, $query2);
     while ($r = pg_fetch_row($result2)) {
-           echo $r["c_uid"];
+           echo $r;
         }
     
         $output = array(
