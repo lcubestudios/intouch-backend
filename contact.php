7a109ccd993470bd1a325f141f19da171b82cb4e
Luis Munoz <lmunoz0806@gmail.com>
2022-02-19 17:08:17 -0500

    bugs: debugging

--- contact.php
+++ contact.php
@@ -16,11 +16,11 @@ if($method === "GET"){
         $u_id = $row['u_id']
     
     // Load User Contacts 
-    $query2 = "SELECT c_uid FROM " . $contacts_table. " WHERE u_id = '". $u_id. "'";
-    $data = pg_query($conn, $query2);
-    while ($row2 = pg_fetch_row($data)){
-        var_dump($row2);
-    }
+    // $query2 = "SELECT c_uid FROM " . $contacts_table. " WHERE u_id = '". $u_id. "'";
+    // $data = pg_query($conn, $query2);
+    // while ($row2 = pg_fetch_row($data)){
+    //     var_dump($row2);
+    // }
         $output = array(
             'status_code' => 200,
             'id' => $u_id
