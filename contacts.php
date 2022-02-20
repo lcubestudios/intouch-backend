365e4fcabcc6c72ab341b23369cd393b7ed8b47c
Luis Munoz <lmunoz0806@gmail.com>
2022-02-20 13:58:29 -0500

    testing delete contact api

--- contacts.php
+++ contacts.php
@@ -107,17 +107,29 @@ switch ($method):
         $raw=file_get_contents('php://input');
         $data=json_decode($raw,true);
         $token = $data['token'];
-        $c_uid = $data['phone_number'];
+        $phone_number = $data['phone_number'];
     
         $query = "SELECT u_id FROM " . $users_table ." WHERE token = '".$token."';";
         $result = pg_query($conn, $query);
     
         if($row = pg_fetch_row($result)) {
                 $u_id  = $row[0];
-                #echo $u_id;
         }
-        $delete_relationship="DELETE FROM " . $contacts_table ." WHERE u_id = '".$u_id."' ;";
+        //getting the c_id from phone_number
+        $query2 = "SELECT u_id FROM " . $table ." WHERE phone_number = '".$phone_number."';";
+        $result2 = pg_query($conn, $query2);
+        while ($row = pg_fetch_row($result2)) {
+            $c_id  = $row[0];
+        }
+
+        $delete_relationship="DELETE FROM " . $table2 ." WHERE (u_id = '".$u_id."' AND c_uid = '".$c_id."');";
         $result = pg_query($conn, $delete_relationship);
+         
+        //Output Contacts
+        $output = array(
+            'status_code' => 200,
+            'message' => 'Contact has been deleted!'
+        );
         pg_close($conn);
 endswitch;
 ?>
