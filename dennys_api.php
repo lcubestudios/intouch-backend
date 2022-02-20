ceb2798e2ac9bdb3bcdbedb3fe5ff56a43895321
dennys9415 <dennys9415@gmail.com>
2022-02-20 11:46:16 -0500

    dennys api delete relationship updated

--- dennys_api.php
+++ dennys_api.php
@@ -12,6 +12,7 @@ switch ($method):
         echo("Deleting the user guy \n");
         DeleteRelationship();
         //DeleteContact();
+        //DeleteMessages();
         break;
 endswitch;
 
@@ -109,19 +110,51 @@ function DeleteRelationship(){
     $raw=file_get_contents('php://input');
     $data=json_decode($raw,true);
     $token = $data['token'];
+    $phone_number = $data['phone_number'];
 
     $query = "SELECT u_id FROM " . $table ." WHERE token = '".$token."';";
     $result = pg_query($conn, $query);
-
     while ($row = pg_fetch_row($result)) {
             $u_id  = "$row[0]\n";
             echo "the u_id is: ";
             echo $u_id;
         }
-    $delete_relationship="DELETE FROM " . $table2 ." WHERE u_id = '".$u_id."' ;";
+    $query2 = "SELECT u_id FROM " . $table ." WHERE phone_number = '".$phone_number."';";
+    $result2 = pg_query($conn, $query2);
+    while ($row = pg_fetch_row($result2)) {
+            $c_id  = "$row[0]\n";
+            echo "the u_id is: ";
+            echo $c_id;
+        }
+
+    $delete_relationship="DELETE FROM " . $table2 ." WHERE (u_id = '".$u_id."' AND c_uid = '".$c_id."');";
     $result = pg_query($conn, $delete_relationship);
     echo "deleting the relationship of: ";
-    echo $token;
+    echo $u_id;
+    echo " and ";
+    echo $c_id;
 
+}
+
+function DeleteMessages(){
+    require('./config.php');
+    $table = 'public.users';
+    $table3 = 'public.messages';
+
+    $raw=file_get_contents('php://input');
+    $data=json_decode($raw,true);
+    $token = $data['token'];
+    $c_id = $data['c_id'];
+
+    //Get own ID with Token 
+    $query = "SELECT u_id FROM " . $table ." WHERE token = '".$token."';";
+    $result = pg_query($conn, $query);
+    $row = pg_fetch_row($result);
+    $u_id = "$row[0]";
+    //get all messages
+    $delete_messages = "DELETE FROM " . $table3 ." WHERE (s_id = '".$u_id."' AND r_id = '".$c_id."') OR (r_id = '".$u_id."' AND s_id = '".$c_id."');";
+    $result2 = pg_query($conn, $delete_messages);
+    echo "deleting all the messages";
+       
 }
 ?>
