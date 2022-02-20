cb240b445ba5a416c82ab0acaf76992e643228e3
dennys9415 <dennys9415@gmail.com>
2022-02-20 14:23:31 -0500

    count message function dennys api

--- dennys_api.php
+++ dennys_api.php
@@ -5,7 +5,8 @@ switch ($method):
     case 'GET':
         echo "";
         //Getting();
-        GetMessages();
+        //GetMessages();
+        GettingTotalUnread();
         echo"";
         break;    
     case 'DELETE':
@@ -50,6 +51,40 @@ function Getting(){
         echo "\n";
     }
 
+}
+
+function GettingTotalUnread(){
+    require('./config.php');
+    $table = 'public.users';
+    $table3 = 'public.messages';
+
+    $raw=file_get_contents('php://input');
+    $data=json_decode($raw,true);
+    $token = $data['token'];
+    $phone_number = $data['phone_number'];
+
+    //Get own ID with Token 
+    $query = "SELECT u_id FROM " . $table ." WHERE token = '".$token."';";
+    $result = pg_query($conn, $query);
+    while ($row = pg_fetch_row($result)) {
+        $u_id = "$row[0]";
+    }
+
+    //getting the c_id from phone_number
+    $query2 = "SELECT u_id FROM " . $table ." WHERE phone_number = '".$phone_number."';";
+    $result2 = pg_query($conn, $query2);
+    while ($row = pg_fetch_row($result2)) {
+            $c_id  = "$row[0]\n";
+    }
+    $query3 = "SELECT COUNT(*) FROM " . $table3 ." WHERE (r_read = FALSE AND s_id = '". $u_id."' AND r_id = '". $c_id."');";
+    //$query3 = "SELECT u_id FROM " . $table ." WHERE phone_number = '".$phone_number."';";
+    $result3 = pg_query($conn, $query3);
+    $row = pg_fetch_row($result3);
+    echo "the number of unread messages from $u_id to $c_id is: ";
+    echo "$row[0]\n";
+    //SELECT COUNT(*) FROM "public"."messages" WHERE r_read = FALSE;
+    //SELECT COUNT(*) FROM "public"."messages" WHERE (r_read = FALSE AND s_id = 1 AND r_id = 3);
+    
 }
 
 function GetMessages(){
