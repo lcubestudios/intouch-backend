26c1773c83ebc63024a86a2dd4f176b017767f12
Luis Munoz <lmunoz0806@gmail.com>
2022-02-20 17:19:44 -0500

    testing get messages

--- messages.php
+++ messages.php
@@ -16,6 +16,56 @@ require('./config.php');
 // }
 
 switch ($method):
+   case 'GET':
+      $users_table = 'public.users';
+      $messages_table = 'public.messages';
+
+      //Load Contact information
+      $raw=file_get_contents('php://input');
+      $data=json_decode($raw,true);
+      $token = $data['token'];
+      $phone_number = $data['phone_number'];
+
+       //Get own ID with Token 
+      $load_id = "SELECT u_id FROM " . $users_table ." WHERE token = '".$token."';";
+      $result = pg_query($conn, $load_id);
+      while ($row = pg_fetch_row($result)) {
+         $u_id = $row[0];
+      }
+      //Load Reciever user id
+      $load_reciever_id = "SELECT u_id FROM " . $users_table ." WHERE phone_number = '".$phone_number."'";
+      $result2 = pg_query($conn, $load_reciever_id);
+      if($r = pg_fetch_row($result2)) {
+         $r_uid = $r[0];
+         $r_first_name = $r[1];
+         $r_last_name = $r[2];
+      }
+      //Update READ field
+      $update_read_field = "UPDATE " . $messages_table ." SET r_read = TRUE WHERE (s_id = '".$u_id."' AND r_id = '".$c_id."');";
+      pg_query($conn, $update_read_field);
+
+      //get all messages
+      $load_messages = "SELECT * FROM " . $messages_table ." WHERE (s_id = '".$u_id."' AND r_id = '".$c_id."') OR (r_id = '".$u_id."' AND s_id = '".$c_id."');";
+      $results = pg_query($conn, $load_messages);
+      $messages_array = array();
+
+      while ($row = pg_fetch_row($results)) {
+         array_push($messages_array, array( 
+         "sender_id" = $row[0],
+         "receiver_id" = $row[1],
+         "body_text" = $row[2],
+         "messages_read" = $row[3],
+         "date_message" = $row[4],
+         "message_id" = $row[5]
+         ));
+      }
+      //Output Contacts
+      $output = array(
+         'status_code' => 200,
+         'contacts' => $messages_array
+      );
+  
+   break;
    case 'POST':
       $users_table = 'public.users';
       $contacts_table = 'public.contacts';
@@ -49,7 +99,7 @@ switch ($method):
       //Output Contacts
       $output = array(
          'status_code' => 200,
-         'message' => "new message"
+         'message' => "message sent"
      );
    echo json_encode($output);
 	pg_close($conn);
