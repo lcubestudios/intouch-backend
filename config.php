0c1842e7c7ed62ebb6e8a01fe7f2a368ad46d3fa
Luis Munoz <lmunoz0806@gmail.com>
2022-04-19 20:11:03 -0400

    reverting to load

--- config.php
+++ config.php
@@ -13,8 +13,7 @@ require_once realpath(__DIR__ . '/vendor/autoload.php');
 
 // Looing for .env at the root directory
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-#$dotenv->load();
-$dotenv->overload();
+$dotenv->load();
 
 // Retrive env variable
 $host = $_ENV['DB_HOST'];
